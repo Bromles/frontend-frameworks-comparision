d9970f6b66026c8f69255cb7ee541b7a5ab21c28
Bromles <alexandr.v.u@gmail.com>
2022-08-04 04:35:47 +0300

    complete vue projects setup

--- vue-options/src/components/HelloWorld.vue
+++ vue-options/src/components/HelloWorld.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  props: {
+    msg: String
+  },
+}
 </script>
 
 <template>
